ecfa8559f93e7d55b949724c9d32bf67831a5313
AzureAD\AlbinBerg <albin.berg@2550.engineering>
2022-02-04 10:44:18 +0100

    base template
    based on the Academind beginner crash course project. Additional tweaks aoplied

---
+++ src/pages/CustomPage.js
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+
+
+import MeetupList from '../components/meetups/MeetupList';
+
+function CustomPage() {
+  
+
+  let content;
+
+
+  
+
+  return (
+    <section>
+      <h1>My Custom Page</h1>
+      {content}
+    </section>
+  );
+}
+
+export default CustomPage;
