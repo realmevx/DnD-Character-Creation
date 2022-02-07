40b1bdedf836db56d07114d9900fcc03f5ad0f6b
AzureAD\AlbinBerg <albin.berg@2550.engineering>
2022-02-07 10:24:12 +0100

    more options
    Added additional options, and css tweaks

--- src/components/characters/NewCharacterForm.js
+++ src/components/characters/NewCharacterForm.js
@@ -9,6 +9,19 @@ function NewCharacterForm(props) {
   const addressInputRef = useRef();
   const descriptionInputRef = useRef();
 
+  const divStyle = {
+    display: 'inline-block',
+    margin:'2px',
+    
+  };
+
+  const tdStyle = {
+    
+    'white-space':'nowrap'
+    
+  };
+  
+
   function submitHandler(event) {
     event.preventDefault();
 
@@ -38,10 +51,20 @@ function NewCharacterForm(props) {
           <label htmlFor='image'>Character Image</label>
           <input type='url' required id='image' ref={imageInputRef} />
         </div>
-        <div className={classes.control}>
-          <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' ref={addressInputRef} />
+        <td style={tdStyle}>
+        <div className={classes.control} style={divStyle}>
+          <label htmlFor='race'>Race</label>
+          <input type='text' required id='race' ref={addressInputRef} />
         </div>
+        <div className={classes.control} style={divStyle}>
+          <label htmlFor='alignment'>Alignment</label>
+          <input type='text' required id='alignment' ref={addressInputRef} />
+        </div>
+        <div className={classes.control} style={divStyle}>
+          <label htmlFor='class'>Class</label>
+          <input type='text' required id='class' ref={addressInputRef} />
+        </div>
+        </td>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
           <textarea
@@ -51,6 +74,47 @@ function NewCharacterForm(props) {
             ref={descriptionInputRef}
           ></textarea>
         </div>
+        <div style={divStyle}className={classes.control}>
+        <label for="Dexterity">Dexterity</label>
+        <input type="number" id="dexterity" name="dexterity"
+       min="1" max="10" ></input>
+       
+        </div>
+        <div style={divStyle}className={classes.control}>
+        <label for="Strength">Strength</label>
+        <input type="number" id="strength" name="strength"
+       min="1" max="10" ></input>
+       
+        </div>
+
+        <div style={divStyle}className={classes.control}>
+        <label for="Charisma">Charisma</label>
+        <input type="number" id="charisma" name="charisma"
+       min="1" max="10" ></input>
+       
+        </div>
+
+        <div style={divStyle}className={classes.control}>
+        <label for="Intelligence">Intelligence</label>
+        <input type="number" id="intelligence" name="intelligence"
+       min="1" max="10" ></input>
+       
+        </div>
+
+        <div style={divStyle}className={classes.control}>
+        <label for="Wisdom">Wisdom</label>
+        <input type="number" id="wisdom" name="wisdom"
+       min="1" max="10" ></input>
+       
+        </div>
+
+        <div style={divStyle}className={classes.control}>
+        <label for="Constitution">Constitution</label>
+        <input type="number" id="constitution" name="constitution"
+       min="1" max="10" ></input>
+       
+        </div>
+
         <div className={classes.actions}>
           <button>Add Character</button>
         </div>
